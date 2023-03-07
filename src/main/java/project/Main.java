e34d1cf36d5a852195bed24b3daea5a429a27967
Mateusz Preizner <preizner.mateusz@gmail.com>
2023-03-07 19:59:07 +0100

    Finished Sing up function

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -2,13 +2,18 @@ package project;
 
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     static boolean isLoggedIn = false;
 
     public static EntityManager entityManager = HibernateUtil.getSessionFactory().createEntityManager();
+
+
 
     public static void loggingIn() {
         Scanner s = new Scanner(System.in);
@@ -27,7 +32,27 @@ public class Main {
                 System.out.println("Incorrect password or login");
             }
         } else if (chosenOption.equals("Sign up")) {
-
+            final String reqex = "^(.+)@(.+)$";   //^ - oznacza że na początku,(.+) - dowolny symbol użyty co najmniej raz, $ - sprawdzenie końca linii
+            System.out.println("Enter your email: ");
+            String newEmail = s.nextLine();
+            Pattern pattern = Pattern.compile(reqex);
+            Matcher matcher = pattern.matcher(newEmail);
+            if (!matcher.matches()){
+                System.out.println("Unacceptable email format");
+                return;
+            }
+            if (!isAlreadySignedUp(newEmail).isEmpty()){
+                System.out.println("There already is a user with email: " + newEmail);
+                return;
+            }
+            System.out.println("Enter password: ");
+            String newPassword = s.nextLine();
+            if (newPassword.isEmpty()){
+                System.out.println("Please enter a password");
+                return;
+            }
+            createNewUser(newEmail,newPassword);
+            System.out.println("You have successfully created your account, you may now log in on main site");
         }else {
             System.out.println("Incorrect action");
         }
@@ -38,6 +63,20 @@ public class Main {
                 .setParameter("loginInput", loginInput)
                 .getSingleResult();
     }
+    public static List<User> isAlreadySignedUp(String newEmail){
+        return  entityManager.createQuery("from User u where u.login = :newEmail", User.class)
+                .setParameter("newEmail",newEmail )
+                .getResultList();
+    }
+    public static void createNewUser(String newEmail, String newPassword){
+        entityManager.getTransaction().begin();
+        User user = new User();
+        user.setLogin(newEmail);
+        user.setPassword(newPassword);
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+
+    }
     public static void main(String[] args) {
         entityManager.getTransaction().begin();
         User user1 = new User("jan@gmail.com","12345",2000,"jan","Kowalski","987654321", new ArrayList<>());
@@ -45,7 +84,7 @@ public class Main {
         entityManager.persist(user1);
         entityManager.persist(user2);
         entityManager.getTransaction().commit();
-        //loggingIn();
+        loggingIn();
         System.out.println("Log in: " + isLoggedIn);
 
 
