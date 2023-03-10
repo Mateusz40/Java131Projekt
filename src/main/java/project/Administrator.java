796b610b672a5fc61d065354dce10cc30fe11a7b
Mateusz Preizner <preizner.mateusz@gmail.com>
2023-03-10 14:47:15 +0100

    In process of creating user menu, done with part 1 - Change of account data

--- src/main/java/project/Administrator.java
+++ src/main/java/project/Administrator.java
@@ -1,34 +1,23 @@
 package project;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 @Entity
 public class Administrator extends User{
+    @Column
+    @Enumerated(EnumType.STRING)
     private final UserType userType = UserType.ADMINISTRATOR;
-
+    public Administrator() {
+    }
     @Override
     public UserType getUserType() {
         return userType;
     }
 
-    public Administrator() {
-    }
-
-    public Administrator(String login, String password, long wallet, String name, String surname, String passportNumber, List<Ticket> ticketWallet) {
-        super(login, password, wallet, name, surname, passportNumber, ticketWallet);
-    }
-
-    //    public static ServiceAssistant makeNewServiceAssistant(String login, String password, long wallet, String name, String surname, String passportNumber, ArrayList<Ticket> ticketWallet){
-//        return new ServiceAssistant( login, password,wallet,name, surname, passportNumber, ticketWallet);
-//    }
-
-
-//    public Administrator(String login, String password, long wallet, String name, String surname, String passportNumber, ArrayList<Ticket> ticketWallet) {
-//        super(login, password, wallet, name, surname, passportNumber, ticketWallet);
-//    }
 
 
 }
--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,8 +1,10 @@
 package project;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,6 +14,7 @@ public class Main {
     static boolean isLoggedIn = false;
     static boolean isAdministrator = false;
     static boolean isServiceAssistant = false;
+    static User loggedUser;
 
     public static EntityManager entityManager = HibernateUtil.getSessionFactory().createEntityManager();
 
@@ -21,7 +24,8 @@ public class Main {
         Scanner s = new Scanner(System.in);
         System.out.println("Select from the following actions: \n Log in \n Sign up");
         String chosenOption = s.nextLine();
-        if (chosenOption.equals("Log in")) {
+        if (chosenOption.equalsIgnoreCase("Log in")) {
+
             System.out.println("Please insert your login");
             String loginInput = s.nextLine();
             if (isAlreadySignedUp(loginInput).isEmpty()){
@@ -36,17 +40,22 @@ public class Main {
                     case SERVICEASSISTANT -> {
                         isServiceAssistant = true;
                         isLoggedIn = true;
+                        loggedUser = temporaryUser;
                     }
                     case ADMINISTRATOR -> {
                         isAdministrator = true;
                         isLoggedIn = true;
+                        loggedUser = temporaryUser;
                     }
-                    default -> isLoggedIn = true;
+                    default -> {
+                        isLoggedIn = true;
+                        loggedUser = temporaryUser;
+                    }
                 }
             } else {
                 System.out.println("Incorrect password or login");
             }
-        } else if (chosenOption.equals("Sign up")) {
+        } else if (chosenOption.equalsIgnoreCase("Sign up")) {
             final String reqex = "^(.+)@(.+)$";   //^ - oznacza że na początku,(.+) - dowolny symbol użyty co najmniej raz, $ - sprawdzenie końca linii
             System.out.println("Enter your email: ");
             String newEmail = s.nextLine();
@@ -72,6 +81,82 @@ public class Main {
             System.out.println("Incorrect action");
         }
     }
+    public static void userMenu(User loggedUser){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello " + loggedUser.getName() + " \n Select an action number from the following list: \n" +
+                " 1. Change of account data \n 2. Purchase, or return of a ticket \n 3. Account crediting \n 4. Contact a flight assistant");
+        System.out.print("Action number: ");
+        int actionNumber = scanner.nextInt();
+        switch (actionNumber){
+            case 1 -> {
+                System.out.println("Which information would you like to change: \n Name \n Surname \n Password \n Passport");
+                String actionType = scanner.next();
+                switch (actionType){
+                    case "name" -> {
+                        System.out.println("Please enter new name: ");
+                        loggedUser.setName(scanner.next());
+                        entityManager.getTransaction().begin();
+                       Query query = entityManager.createQuery("update User u set u.name = :new where u.login = :login")
+                                .setParameter("new", loggedUser.getName())
+                                .setParameter("login", loggedUser.getLogin());
+                       int result = query.executeUpdate();
+                        entityManager.getTransaction().commit();
+                        System.out.println("Your name was successfully changed");
+                        System.out.println("Your name is from now: " + loggedUser.getName());
+                    }
+                    case "surname" ->{
+                        System.out.println("Please enter new surname: ");
+                        loggedUser.setSurname(scanner.next());
+                        entityManager.getTransaction().begin();
+                        Query query = entityManager.createQuery("update User u set u.surname = :new where u.login = :login")
+                                .setParameter("new", loggedUser.getSurname())
+                                .setParameter("login", loggedUser.getLogin());
+                        entityManager.getTransaction().commit();
+                        System.out.println("Your surname was successfully changed");
+                        System.out.println("Your surname is from now: " + loggedUser.getSurname());
+                    }
+                    case "password" ->{
+                        System.out.println("Please enter new password");
+                        String temporaryPassword = scanner.next();
+                        if (loggedUser.getPassword().equals(temporaryPassword)){
+                            System.out.println("You already have this password");
+                        }else loggedUser.setPassword(temporaryPassword);
+                        entityManager.getTransaction().begin();
+                        Query query = entityManager.createQuery("update User u set u.password = :new where u.login = :login")
+                                .setParameter("new", loggedUser.getPassword())
+                                .setParameter("login", loggedUser.getLogin());
+                        int result = query.executeUpdate();
+                        entityManager.getTransaction().commit();
+                        System.out.println("Your password was successfully changed");
+                        System.out.println("Your password is from now: " + loggedUser.getPassword());
+                    }
+                    case "passport" ->{
+                        System.out.println("Please enter your passport number: ");
+                        loggedUser.setPassportNumber(scanner.next());
+                        entityManager.getTransaction().begin();
+                        Query query = entityManager.createQuery("update User u set u.passportNumber = :new where u.login = :login")
+                                .setParameter("new", loggedUser.getPassportNumber())
+                                .setParameter("login", loggedUser.getLogin());
+                        int result = query.executeUpdate();
+                        entityManager.getTransaction().commit();
+                        System.out.println("Your passport number was successfully changed");
+                        System.out.println("Your passport number is from now: " + loggedUser.getPassportNumber());
+                    }
+                    default -> System.out.println("Declared incorrect action");
+                }
+            }
+            case 2 -> {
+                Ticket ticket = new Ticket();
+                ticket.buyTicket(loggedUser);
+            }
+            case 3 -> {
+                loggedUser.addToWallet(scanner.nextLong());
+            }
+            case 4 -> {
+
+            }
+        }
+    }
 
     public static User loginUser(String loginInput){
         return entityManager.createQuery("from User u where u.login = :loginInput", User.class)
@@ -95,7 +180,7 @@ public class Main {
     public static void main(String[] args) {
         entityManager.getTransaction().begin();
         User user1 = new User("jan@gmail.com","12345",2000,"jan","Kowalski","987654321", new ArrayList<>());
-        User user2 = new User("adam@gmail.com","12345",2000,"jan","Kowalski","987654321", new ArrayList<>());
+        User user2 = new User("adam@gmail.com","12345",500,"adam","Siekera","123456789", new ArrayList<>());
         Administrator administrator = new Administrator();
         administrator.setLogin("admin");
         administrator.setPassword("admin");
@@ -110,9 +195,12 @@ public class Main {
         while(!isLoggedIn) {
             loggingIn();
         }
+        while(isLoggedIn && !isServiceAssistant && !isAdministrator){
+            userMenu(loggedUser);
+        }
         System.out.println("Log in: " + isLoggedIn);
         System.out.println("service assistant = " + isServiceAssistant);
-        System.out.println("administator = " + isAdministrator);
+        System.out.println("administrator = " + isAdministrator);
 
 
 
